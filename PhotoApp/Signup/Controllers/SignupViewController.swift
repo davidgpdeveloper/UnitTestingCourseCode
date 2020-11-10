a4f09fb8f3cba99c0a26fad2dabec5ee4461fe26
David Galán <david.galan@studiogenesis.es>
2020-11-10 11:04:58 +0100

    Video 82 finished.

--- PhotoApp/Signup/Controllers/SignupViewController.swift
+++ PhotoApp/Signup/Controllers/SignupViewController.swift
@@ -14,22 +14,15 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var userEmailTextField: UITextField!
     @IBOutlet weak var userPasswordNameTextField: UITextField!
     @IBOutlet weak var userRepeatPasswordNameTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
     
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
